3080ff53184f68e5e56f087804f0c075276dacba
danula2008 <danusathmin@gmail.com>
2024-09-06 23:39:42 +0530

    implemented 1st api

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-let code_weather_details = "";
-
-for (let i = 0; i < 9; i++) {
-  code_weather_details += `
-    <div class="col fw-medium">
-        <div class="bg-black p-2 rounded-2 py-3">
-            <p style="color: #7D7878; font-size: 15px;">Precipitation</p>
-            <h6 class="fs-25">0.1 mm</h6>
-        </div>
-    </div>`;
-}
-
-document.getElementById("weather_details_grid").innerHTML =
-  code_weather_details;
-
 let code_hourly_forcast = "";
 
 for (let i = 0; i < 24; i++) {
@@ -25,4 +10,88 @@ for (let i = 0; i < 24; i++) {
 
 document.getElementById("hourly_forcast_grid").innerHTML = code_hourly_forcast;
 
-document.getElementById('datePicker').value = new Date().toISOString().slice(0, 10);
+document.getElementById("datePicker").value = new Date()
+  .toISOString()
+  .slice(0, 10);
+
+// -----------------------------------------------------------------------------------
+
+fetch(
+  "https://api.weatherapi.com/v1/current.json?q=sri-lanka&key=ead3f37c992946bb943145314240609"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    setData(data)
+  })
+  .catch((error) => console.error("Error:", error));
+
+
+function setData(data){
+  document.getElementById("name_region").innerText = data.location.name + ", " + data.location.country;
+  document.getElementById("latitude").innerText = "Latitude: " + data.location.lat + "°N";
+  document.getElementById("longitude").innerText = "Longitude: " + data.location.lon + "°W";
+  document.getElementById("last_updated").innerText = "Last update: " + data.current.last_updated;
+  document.getElementById("temp").innerText = data.current.temp_c + "°C";
+  document.getElementById("condition_txt").innerText = data.current.condition.text;
+  document.getElementById("condition_icon").src = "https:" + data.current.condition.icon;
+  document.getElementById("condition_icon").alt = data.current.condition.text;
+  document.getElementById("cloud").innerText = "Cloud: " + data.current.cloud + "%";
+  document.getElementById("wind_dir").innerText = "Wind direction: " + data.current.wind_dir;
+
+  setWeatherDetails([
+    [
+      "Feels Like",
+      data.current.feelslike_c + "°C"
+    ],
+    [
+      "Wind Speed",
+      data.current.wind_kph + " km/h"
+    ],
+    [
+      "Humidity",
+      data.current.humidity + "%"
+    ],
+    [
+      "UV",
+      data.current.uv
+    ],
+    [
+      "Visibility",
+      data.current.vis_km + " km"
+    ],
+    [
+      "Pressure",
+      data.current.pressure_mb + " mb"
+    ],
+    [
+      "Precipitation",
+      data.current.precip_mm + " mm"
+    ],
+    [
+      "Gusts",
+      data.current.gust_kph + " km/h"
+    ],
+    [
+      "Dew Point",
+      data.current.dewpoint_c + "°C"
+    ],
+  ])
+}
+
+function setWeatherDetails(data){
+  let code_weather_details = "";
+
+for (let i = 0; i < data.length; i++) {
+  code_weather_details += `
+    <div class="col fw-medium">
+        <div class="bg-black p-2 rounded-2 py-3">
+            <p style="color: #7D7878; font-size: 15px;">${data[i][0]}</p>
+            <h6 class="fs-25">${data[i][1]}</h6>
+        </div>
+    </div>`;
+}
+
+document.getElementById("weather_details_grid").innerHTML =
+  code_weather_details;
+}
